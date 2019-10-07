e28571d3ebe5a1359750ba01f44533a54ed79a74
Lisa <annalisa.quiamco@gmail.com>
2019-10-06 19:30:43 -0600

    Updated reviews page

--- page-reviews.php
+++ page-reviews.php
@@ -14,7 +14,7 @@ get_header(); ?>
 				<header class="pageheader cf">
 					<h1 class="pagetitle"><span><?php the_title(); ?></span></h1>
 				</header>
-				<div class="default-content"><?php the_content(); ?></div>	
+				<div class="default-content cf"><?php the_content(); ?></div>	
 
 			<?php endwhile;  ?>
 

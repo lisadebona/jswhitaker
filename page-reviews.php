9e0fb2a93d039a540d2dc6a7d945260bb7e74bee
Lisa <annalisa.quiamco@gmail.com>
2019-10-06 19:21:53 -0600

    Styling reviews page

---
+++ page-reviews.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Reviews
+ *
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area pagereviews  cf">
+	<main id="main" class="site-main wrapper cf" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+				
+				<header class="pageheader cf">
+					<h1 class="pagetitle"><span><?php the_title(); ?></span></h1>
+				</header>
+				<div class="default-content"><?php the_content(); ?></div>	
+
+			<?php endwhile;  ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
